20535d8e1fbbc85e422bc2d19f6e27a614973949
burkes6 <burkes6@nationwide.com>
2018-02-20 10:00:45 -0500

    put in safe gaurds in case the browser crashes

--- generate_data.rb
+++ generate_data.rb
@@ -9,14 +9,21 @@ player_list = player_html.scan(/\">(.*)<\/a>/i)
 number_of_players = player_list.length
 puts number_of_players
 for i in 2..number_of_players
-  browser.input(:class => 'search js-input-player base-input').click
-  browser.li(:xpath => "//*[@id=\"filter\"]/div/div[2]/section/div/div/div/div/li[#{i}]").click
-  while 1
-    sleep(1)
-    break if !browser.i(:xpath => '//*[@id="loading"]/i').visible?
-    puts(browser.i(:xpath => '//*[@id="loading"]/i').visible?)
-    sleep(1)
+  begin
+    browser.input(:class => ['search js-input-player base-input']).click
+    browser.li(:xpath => "//*[@id=\"filter\"]/div/div[2]/section/div/div/div/div/li[#{i}]").click
+    while 1
+      sleep(1)
+      break if !browser.i(:xpath => '//*[@id="loading"]/i').visible?
+      puts(browser.i(:xpath => '//*[@id="loading"]/i').visible?)
+      sleep(1)
+    end
+  rescue
+    browser.close
+    browser = Watir::Browser.new
+    browser.goto 'https://fantasy.mlssoccer.com/#stats-center/player-profile/86140'
+    i -= 1
   end
+  puts i
 end
-sleep(100)
 browser.close
