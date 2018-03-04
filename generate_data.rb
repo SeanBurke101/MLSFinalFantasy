78b90362c74c3918f56546b2920f355523f2498e
burkes6 <burkes6@nationwide.com>
2018-03-04 15:10:59 -0500

    more changes

--- generate_data.rb
+++ generate_data.rb
@@ -8,22 +8,28 @@ player_html = browser.section(:class => 'player-select-item').html
 player_list = player_html.scan(/\">(.*)<\/a>/i)
 number_of_players = player_list.length
 puts number_of_players
-for i in 2..number_of_players
+for i in 1..number_of_players
   begin
     browser.input(:class => ['search js-input-player base-input']).click
     browser.li(:xpath => "//*[@id=\"filter\"]/div/div[2]/section/div/div/div/div/li[#{i}]").click
     while 1
       sleep(1)
       break if !browser.i(:xpath => '//*[@id="loading"]/i').visible?
-      puts(browser.i(:xpath => '//*[@id="loading"]/i').visible?)
       sleep(1)
     end
+    name = browser.p(:class => ["profile-name"]).text
+    team = browser.p(:class => ["player-team"]).text
+    position_worth = browser.p(:class => ["player-pos uppercase"]).text.split(' ')
+    position = position_worth[0]
+    worth = position_worth[-1]
+    status = browser.p(:class => ["player-pos uppercase"]).html.scan(/<i class=\"(.*)\">/)[0][0]
+    previous_points = browser.p(:xpath => '//*[@id="player-content"]/div/div[2]/div/div/div[1]/div[3]/p[2]').text
+    puts("#{name},#{team},#{position},#{previous_points},#{worth},#{status}")
   rescue
     browser.close
     browser = Watir::Browser.new
     browser.goto 'https://fantasy.mlssoccer.com/#stats-center/player-profile/86140'
     i -= 1
   end
-  puts i
 end
 browser.close
